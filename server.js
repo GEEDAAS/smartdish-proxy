3cfe6e2d0bfce9892cdc1271da01163ffe19f769
GEEDAAS <gerardoguerrerof21@gmail.com>
2025-05-15 03:56:34 -0600

    NuevoAppGet

--- server.js
+++ server.js
@@ -7,21 +7,22 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Permitir CORS desde tu dominio de Firebase
-app.use(cors({
-  origin: 'https://smartdish-firebase.web.app'  // ⚠️ Reemplaza con tu dominio exacto si cambia
-}));
-
 app.get('/captura', async (req, res) => {
+  const urlNgrok = 'https://cd13-2806-2f0-6000-a2ce-8425-bc28-45f1-618f.ngrok-free.app/captura.jpg';
   try {
-    const response = await fetch('https://cd13-2806-2f0-6000-a2ce-8425-bc28-45f1-618f.ngrok-free.app/captura.jpg', {
+    console.log(`Intentando acceder a: ${urlNgrok}`);
+
+    const response = await fetch(urlNgrok, {
       headers: {
         'ngrok-skip-browser-warning': 'true'
       }
     });
 
+    console.log(`Status de respuesta: ${response.status}`);
+
     if (!response.ok) {
       const text = await response.text();
-      console.error("Error en respuesta:", response.status, text);
+      console.error("Contenido de error:", text);
       return res.status(500).send('Error al obtener la imagen desde ngrok');
     }
 
@@ -30,10 +31,11 @@ app.get('/captura', async (req, res) => {
     const buffer = await response.buffer();
     res.send(buffer);
   } catch (err) {
-    console.error("Error general:", err);
-    res.status(500).send('Error al obtener la imagen');
+    console.error("Error general al hacer fetch desde ngrok:", err);
+    res.status(500).send('Error al obtener la imagen (excepción)');
   }
 });
+
 
 
 app.listen(PORT, () => {
